3e30976e88330a435026ebb7e3929087874afa49
Sourav Das <iamsouravdaswade420@gmail.com>
2025-03-21 22:11:31 +0530

    hero gsap animation

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -1,7 +1,39 @@
 import React from 'react';
 import "./css/Hero.css"
 import COMPUTER from "../assets/images/images/computer.png"
+
+import {gsap} from "gsap";
+import {useGSAP} from "@gsap/react";
+
 const Hero = () => {
+
+    useGSAP(() => {
+        let tl = gsap.timeline()
+        tl.from("#heroTop h1", {
+            y: 300,
+            delay: 0.5
+        }, "heroH1")
+        tl.from("#middleLeft h1", {
+            y: 300,
+            delay: 0.5
+        }, "heroH1")
+
+
+        tl.from("#imageBox", {
+            width: 0
+        },"2ndAni")
+        tl.to("#imageBox img", {
+            scale: 1,
+            ease: "expo.out",
+
+        })
+        tl.from("#heroBottom",{
+            width:0
+        },"2ndAni")
+
+    }, {scope: "#heroContainer"})
+
+
     return (
         <div id={"heroContainer"}>
             <div id="heroTop">
