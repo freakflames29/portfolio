78e87d80007576afffec73a7593915ff0168e627
Sourav Das <iamsouravdaswade420@gmail.com>
2025-03-21 21:12:48 +0530

    hero section

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
-
+import "./css/Hero.css"
+import COMPUTER from "../assets/images/images/computer.png"
 const Hero = () => {
     return (
-        <div>
-            
+        <div id={"heroContainer"}>
+            <div id="heroTop">
+                <h1>I turn <span id={"textCircuit"}>Tech</span></h1>
+            </div>
+            <div id="heroMiddle">
+                <div id="middleLeft">
+                    <h1>Into ART</h1>
+                </div>
+                <div id="middleRight">
+                    <div id="imageBox">
+                        <img src={COMPUTER} alt="comp"/>
+                    </div>
+                </div>
+            </div>
+            <div id="heroBottom"></div>
         </div>
     );
 };
--- src/pages/Nav.jsx
+++ src/pages/Nav.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import "./css/Nav.css"
 
-const Nav = () => {
+const Nav = (props) => {
     return (
-        <div>
-            <h1>Nav</h1>
-        </div>
+        <>
+            <div id="navContainer">
+                <nav>
+                    <h1 id="navLogo">Sourav.</h1>
+                    <h1><i class="ri-menu-3-line"></i></h1>
+                </nav>
+            </div>
+
+            {props.children}
+        </>
     );
 };
 
